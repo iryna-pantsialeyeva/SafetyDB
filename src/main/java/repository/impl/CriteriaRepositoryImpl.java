7db829939dca3817c20cd4b9e557fb82c2f3747c
ilya-frolov <ilyafrolov0711@gmail.com>
2022-09-23 10:22:55 +0300

    SafetyDB: - Used try-resources in all methods.

--- src/main/java/repository/impl/CriteriaRepositoryImpl.java
+++ src/main/java/repository/impl/CriteriaRepositoryImpl.java
@@ -13,101 +13,68 @@ public class CriteriaRepositoryImpl implements CriteriaRepository {
     }
 
     public Criteria getByID(int id) {
-        Connection con = ConnectionToDB.connectionPool.getConnection();
         Criteria newCriteria = null;
-        try {
-            PreparedStatement ps = con.prepareStatement(SQLQuery.GET_FROM_CRITERIAS_BY_ID);
+        try (Connection con = ConnectionToDB.connectionPool.getConnection();
+             PreparedStatement ps = con.prepareStatement(SQLQuery.GET_FROM_CRITERIAS_BY_ID);
+             ResultSet rs = ps.executeQuery()) {
+
             ps.setInt(1, id);
-            ResultSet rs = ps.executeQuery();
             if (rs.next()) {
                 newCriteria = new Criteria(rs.getInt("id"), rs.getString("name"));
             }
-            closePS(ps);
-            closeRS(rs);
         } catch (SQLException e) {
             e.printStackTrace();
-        } finally {
-            ConnectionToDB.connectionPool.releaseConnection(con);
         }
         return newCriteria;
     }
 
     public Criteria getByName(String name) {
-        Connection con = ConnectionToDB.connectionPool.getConnection();
         Criteria newCriteria = null;
-        try {
-            PreparedStatement ps = con.prepareStatement(SQLQuery.GET_FROM_CRITERIAS_BY_NAME);
+        try (Connection con = ConnectionToDB.connectionPool.getConnection();
+             PreparedStatement ps = con.prepareStatement(SQLQuery.GET_FROM_CRITERIAS_BY_NAME);
+             ResultSet rs = ps.executeQuery()) {
+
             ps.setString(1, name);
-            ResultSet rs = ps.executeQuery();
             if (rs.next()) {
                 newCriteria = new Criteria(rs.getInt("id"), rs.getString("name"));
             }
-            closePS(ps);
-            closeRS(rs);
         } catch (SQLException e) {
             e.printStackTrace();
-        } finally {
-            ConnectionToDB.connectionPool.releaseConnection(con);
         }
         return newCriteria;
     }
 
     public Criteria add(Criteria criteria) {
-        Connection con = ConnectionToDB.connectionPool.getConnection();
-        try {
-            PreparedStatement ps = con.prepareStatement(SQLQuery.INSERT_IN_CRITERIAS, Statement.RETURN_GENERATED_KEYS);
+        try (Connection con = ConnectionToDB.connectionPool.getConnection();
+             PreparedStatement ps = con.prepareStatement(SQLQuery.INSERT_IN_CRITERIAS, Statement.RETURN_GENERATED_KEYS);
+             ResultSet rs = ps.getGeneratedKeys()) {
+
             ps.setString(1, criteria.getName());
             int updatedRows = ps.executeUpdate();
             System.out.println(updatedRows + " rows were updated in 'authors'.");
-            ResultSet rs = ps.getGeneratedKeys();
+
             if (rs.next()) {
                 criteria.setId(rs.getInt(1));
             }
-            closePS(ps);
-            closeRS(rs);
         } catch (SQLException e) {
             e.printStackTrace();
-        } finally {
-            ConnectionToDB.connectionPool.releaseConnection(con);
         }
         return criteria;
     }
 
     public int getId(String name) {
-        Connection con = ConnectionToDB.connectionPool.getConnection();
         int id = 0;
-        try {
-            PreparedStatement ps = con.prepareStatement(SQLQuery.GET_FROM_CRITERIAS_BY_NAME);
+        try (Connection con = ConnectionToDB.connectionPool.getConnection();
+             PreparedStatement ps = con.prepareStatement(SQLQuery.GET_FROM_CRITERIAS_BY_NAME);
+             ResultSet rs = ps.executeQuery();) {
+
             ps.setString(1, name);
-            ResultSet rs = ps.executeQuery();
             if (rs.next()) {
                 id = rs.getInt("id");
             }
-            closePS(ps);
-            closeRS(rs);
         } catch (SQLException e) {
             e.printStackTrace();
-        } finally {
-            ConnectionToDB.connectionPool.releaseConnection(con);
         }
         return id;
-    }
-
-    private static void closePS(PreparedStatement ps) {
-        if (ps != null) {
-            try {
-                ps.close();
-            } catch (SQLException ignore) {
-            }
-        }
-    }
-
-    private static void closeRS(ResultSet rs) {
-        if (rs != null) {
-            try {
-                rs.close();
-            } catch (SQLException ignore) {
-            }
-        }
     }
 }
--- src/main/java/repository/impl/OutcomeRepositoryImpl.java
+++ src/main/java/repository/impl/OutcomeRepositoryImpl.java
@@ -1,6 +1,5 @@
 package repository.impl;
 
-import model.Criteria;
 import model.Outcome;
 import repository.OutcomeRepository;
 import repository.util.ConnectionToDB;
@@ -10,105 +9,72 @@ import java.sql.*;
 
 public class OutcomeRepositoryImpl implements OutcomeRepository {
 
-    public OutcomeRepositoryImpl() {}
+    public OutcomeRepositoryImpl() {
+    }
 
     public Outcome getByID(int id) {
-        Connection con = ConnectionToDB.connectionPool.getConnection();
         Outcome newOutcome = null;
-        try {
-            PreparedStatement ps = con.prepareStatement(SQLQuery.GET_FROM_OUTCOMES_BY_ID);
+        try (Connection con = ConnectionToDB.connectionPool.getConnection();
+             PreparedStatement ps = con.prepareStatement(SQLQuery.GET_FROM_OUTCOMES_BY_ID);
+             ResultSet rs = ps.executeQuery()) {
+
             ps.setInt(1, id);
-            ResultSet rs = ps.executeQuery();
             if (rs.next()) {
                 newOutcome = new Outcome(rs.getInt("id"), rs.getString("name"));
             }
-            closePS(ps);
-            closeRS(rs);
         } catch (SQLException e) {
             e.printStackTrace();
-        } finally {
-            ConnectionToDB.connectionPool.releaseConnection(con);
         }
         return newOutcome;
     }
 
     public Outcome getByName(String name) {
-        Connection con = ConnectionToDB.connectionPool.getConnection();
         Outcome newOutcome = null;
-        try {
-            PreparedStatement ps = con.prepareStatement(SQLQuery.GET_FROM_OUTCOMES_BY_NAME);
+        try (Connection con = ConnectionToDB.connectionPool.getConnection();
+             PreparedStatement ps = con.prepareStatement(SQLQuery.GET_FROM_OUTCOMES_BY_NAME);
+             ResultSet rs = ps.executeQuery()){
+
             ps.setString(1, name);
-            ResultSet rs = ps.executeQuery();
             if (rs.next()) {
                 newOutcome = new Outcome(rs.getInt("id"), rs.getString("name"));
             }
-            closePS(ps);
-            closeRS(rs);
         } catch (SQLException e) {
             e.printStackTrace();
-        } finally {
-            ConnectionToDB.connectionPool.releaseConnection(con);
         }
         return newOutcome;
     }
 
     public Outcome add(Outcome outcome) {
-        Connection con = ConnectionToDB.connectionPool.getConnection();
-        try {
-            PreparedStatement ps = con.prepareStatement(SQLQuery.INSERT_IN_OUTCOMES, Statement.RETURN_GENERATED_KEYS);
+        try (Connection con = ConnectionToDB.connectionPool.getConnection();
+             PreparedStatement ps = con.prepareStatement(SQLQuery.INSERT_IN_OUTCOMES, Statement.RETURN_GENERATED_KEYS);
+             ResultSet rs = ps.getGeneratedKeys()){
+
             ps.setString(1, outcome.getName());
             int updatedRows = ps.executeUpdate();
             System.out.println(updatedRows + " rows were updated in 'authors'.");
-            ResultSet rs = ps.getGeneratedKeys();
+
             if (rs.next()) {
                 outcome.setId(rs.getInt(1));
             }
-            closePS(ps);
-            closeRS(rs);
         } catch (SQLException e) {
             e.printStackTrace();
-        } finally {
-            ConnectionToDB.connectionPool.releaseConnection(con);
         }
         return outcome;
     }
 
     public int getId(String name) {
-        Connection con = ConnectionToDB.connectionPool.getConnection();
-        int id = 0;
-        try {
-            PreparedStatement ps = con.prepareStatement(SQLQuery.GET_FROM_OUTCOMES_BY_NAME);
+                int id = 0;
+        try (Connection con = ConnectionToDB.connectionPool.getConnection();
+             PreparedStatement ps = con.prepareStatement(SQLQuery.GET_FROM_OUTCOMES_BY_NAME);
+             ResultSet rs = ps.executeQuery();){
+
             ps.setString(1, name);
-            ResultSet rs = ps.executeQuery();
             if (rs.next()) {
                 id = rs.getInt("id");
             }
-            closePS(ps);
-            closeRS(rs);
         } catch (SQLException e) {
             e.printStackTrace();
-        } finally {
-            ConnectionToDB.connectionPool.releaseConnection(con);
         }
         return id;
     }
-
-    private static void closePS(PreparedStatement ps) {
-        if (ps != null) {
-            try {
-                ps.close();
-            } catch (SQLException ignore) {
-            }
-        }
-    }
-
-    private static void closeRS(ResultSet rs) {
-        if (rs != null) {
-            try {
-                rs.close();
-            } catch (SQLException ignore) {
-            }
-        }
-    }
-
 }

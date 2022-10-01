d5bd105f5de9bb6023ae5136e94c3bda76d757db
ilya-frolov <ilyafrolov0711@gmail.com>
2022-10-01 14:44:00 +0300

    SafetyDB: Runner made empty.

--- src/main/java/service/Runner.java
+++ src/main/java/service/Runner.java
@@ -1,12 +1,8 @@
 package service;
 
-import service.impl.ADRServiceImpl;
-
 public class Runner {
 
     public static void main(String[] args) {
 
-        ADRService adr = new ADRServiceImpl();
-        System.out.println(adr.getAll());
     }
 }
